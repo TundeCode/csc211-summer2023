05366a94438b18708780d884117ddde7de8a83a9
Michael Conti <michaelconti@uri.edu>
2023-07-18 14:29:12 -0400

    classes

---
+++ code/Point2D/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "Point.h"
+
+int main()
+{
+
+    Point myPt(10, 5, 8);
+    Point myPt2(3, 10, 15);
+
+    int myX = myPt.getX();
+
+    std::cout << myX << std::endl;
+
+    std::cout << myPt.toCord() << std::endl;
+
+    myPt.setX(3);
+
+    std::cout << myPt.toCord() << std::endl;
+
+
+return 0;
+}
